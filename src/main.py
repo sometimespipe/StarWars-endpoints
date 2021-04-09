6cfd31bd30e74bca62d346422e12006d46ed2354
sometimespipe <andrespipehl@gmail.com>
2021-04-09 01:09:08 +0000

    added endpoints and API own API requests

--- src/main.py
+++ src/main.py
@@ -4,17 +4,17 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
+from flask_swagger import swagger #not used in this exercise
+from flask_cors import CORS #to avoid CORS (Cross-Origin Resource Sharing) domain errors 
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Planet, Favorite
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app = Flask(__name__)    #create new Flask app
+app.url_map.strict_slashes = False    #to allow URL with or without final slash "/"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')  #connect to database specified in file: .env
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   #if "true", everytime I modify models.py it creates a migration
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -25,21 +25,213 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
+### User endpoints [GET, POST, PUT, UPDATE]: 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_user():
+    all_users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), all_users)) 
+    print("GET all_users: ", all_users)
+    return jsonify(all_users), 200
 
+@app.route('/user/<int:id>', methods=['GET'])
+def get_single_user(id):
+    user = User.query.get(id)
+
+    if user is None:
+        raise APIException('User not found', status_code=404)
+
+    print("GET single user: ", user)
+    return jsonify(user.serialize()), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    user = User(username=request_body["username"], email=request_body["email"], password=request_body["password"])
+    db.session.add(user)
+    db.session.commit()
+    print("User created: ", request_body)
+    return jsonify(request_body), 200
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    request_body = request.get_json()
+    user = User.query.get(user_id)
+
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    if "username" in request_body:
+        user.username = request_body["username"]
+    if "email" in request_body:
+        user.email = request_body["email"]
+    if "password" in request_body:
+        user.password = request_body["password"]
+    
+    db.session.commit()
+
+    print("User property updated: ", request_body)
+    return jsonify(request_body), 200
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+
+    if user is None:
+        raise APIException('User not found', status_code=404)
+
+    db.session.delete(user)
+    db.session.commit()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+         "msg": "User delete successful",
     }
-
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is executed
+
+### Character endpoints [GET, POST, PUT, UPDATE]: 
+@app.route('/character', methods=['GET'])
+def get_all_character():
+    all_characters = Character.query.all()
+    all_characters = list(map(lambda x: x.serialize(), all_characters)) 
+    return jsonify(all_characters), 200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_single_character(id):
+    character = Character.query.get(id)
+
+    if character is None:
+        raise APIException('Character not found', status_code=404)
+
+    return jsonify(character.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def create_character():
+    request_body = request.get_json()
+    character = Character(name=request_body["name"], birth_year=request_body["birth_year"],  gender=request_body["gender"], height=request_body["height"], mass=request_body["mass"], home_world=request_body["home_world"])
+    db.session.add(character)
+    db.session.commit()
+    print("Character created: ", request_body)
+    return jsonify(request_body), 200
+
+@app.route('/character/<int:id>', methods=['PUT'])
+def update_character(id):
+    request_body = request.get_json()
+    character = Character.query.get(id)
+
+    if character is None:
+        raise APIException('Character not found', status_code=404)
+    if "name" in request_body:
+        character.name = request_body["name"]
+    if "birth_year" in request_body:
+        character.birth_year = request_body["birth_year"]
+    if "mass" in request_body:
+        character.eye_color = request_body["mass"]
+    if "gender" in request_body:
+        character.gender = request_body["gender"]
+    if "home_world" in request_body:
+        character.hair_color = request_body["home_world"]
+    if "height" in request_body:
+        character.height = request_body["height"]
+    
+    db.session.commit()
+
+    print("Character property updated: ", request_body)
+    return jsonify(request_body), 200
+
+@app.route('/character/<int:id>', methods=['DELETE'])
+def delete_character(id):
+    character = Character.query.get(id)
+
+    if character is None:
+        raise APIException('Character not found', status_code=404)
+
+    db.session.delete(character)
+    db.session.commit()
+    response_body = {
+         "msg": "Character delete successful",
+    }
+    return jsonify(response_body), 200
+
+
+### Planet endpoints [GET, POST, PUT, UPDATE]: 
+@app.route('/planet', methods=['GET'])
+def get_all_planet():
+    all_planets = Planet.query.all()
+    all_planets = list(map(lambda x: x.serialize(), all_planets)) 
+    return jsonify(all_planets), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_single_planet(id):
+    planet = Planet.query.get(id)
+
+    if planet is None:
+        raise APIException('Planet not found', status_code=404)
+
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def create_planet():
+    request_body = request.get_json()
+    planet = Planet(name=request_body["name"], climate=request_body["climate"], orbital_period=request_body["orbita_period"], population=request_body["population"], rotation_period=request_body["rotation_period"], gravity=request_body["gravity"])
+    db.session.add(planet)
+    db.session.commit()
+    print("Planet created: ", request_body)
+    return jsonify(request_body), 200
+
+@app.route('/planet/<int:id>', methods=['PUT'])
+def update_planet(id):
+    request_body = request.get_json()
+    planet = Planet.query.get(id)
+
+    if planet is None:
+        raise APIException('Planet not found', status_code=404)
+    if "name" in request_body:
+        planet.name = request_body["name"]
+    if "climate" in request_body:
+        planet.climate = request_body["climate"]
+    if "gravity" in request_body:
+        planet.diameter = request_body["gravity"]
+    if "population" in request_body:
+        planet.population = request_body["population"]
+    if "rotation_period" in request_body:
+        planet.rotation_period = request_body["rotation_period"]
+    if "orbital_period" in request_body:
+        planet.terrain = request_body["orbital_period"]
+    
+    db.session.commit()
+
+    print("Planet property updated: ", request_body)
+    return jsonify(request_body), 200
+
+@app.route('/planet/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+
+    if planet is None:
+        raise APIException('Planet not found', status_code=404)
+
+    db.session.delete(planet)
+    db.session.commit()
+    response_body = {
+         "msg": "Planet delete successful",
+    }
+    return jsonify(response_body), 200
+
+
+### Favorite endpoints:
+@app.route('/favorite', methods=['GET'])
+def get_all_favorite():
+    all_favorites = Favorite.query.all()
+    all_favorites = list(map(lambda x: x.serialize(), all_favorites)) 
+    return jsonify(all_favorites), 200
+
+
+# These two lines should always be at the end of the main.py file.
+# Meaning: only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,18 +2,96 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# For each 'model` I have to declare a class with the model properties 
+# and a method `serialize` that returns a dictionary representation of the class
+
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    age = db.Column(db.String(80), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref='User') # One to Many
 
+    # __repr__():  tell python how to print the class object in the console
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User: %r>' % self.username
+
+    # serialize(): tell python how convert the class object into a dictionary ready to jsonify
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "age": self.age,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Favorite(db.Model):
+    __tablename__ = "favorite"
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, unique=False, nullable=False) # store character_id or planet_id
+    item_type = db.Column(db.String(80), unique=False, nullable=False) # type can be Character or Planet
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+            "item_id": self.item_id, 
+            "item_type": self.item_type
         }
+
+
+class Character(db.Model):
+    __tablename__ = "character"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    birth_year = db.Column(db.String(50), unique=False, nullable=False)
+    gender = db.Column(db.String(50), unique=False, nullable=False)
+    height = db.Column(db.Float, unique=False, nullable=False)
+    mass = db.Column(db.Float, unique=False, nullable=False)
+    home_world = db.Column(db.String(50), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character: %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "mass": self.mass,
+            "home_world": self.home_world, 
+        }
+
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    population = db.Column(db.Float, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+    gravity = db.Column(db.String(50), unique=False, nullable=False)
+    rotation_period = db.Column(db.Float, unique=False, nullable=False)
+    climate = db.Column(db.String(50), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planet: %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "gravity": self.gravity,
+            "orbital_period": self.orbital_period,
+            "climate": self.climate,
+            "rotation_period": self.rotation_period
+        }
+    
